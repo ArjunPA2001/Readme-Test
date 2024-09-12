895d923cbe2672172379b8a2c697c9e3c94da883
Arjun <arjunpa2001@gmail.com>
2024-09-12 15:57:58 +0530

    Updated documents

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,5 @@
+export default [
+  require("D:\\texttosql\\demo\\Readme-Test\\docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\texttosql\\demo\\Readme-Test\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\texttosql\\demo\\Readme-Test\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+];
---
+++ docs/.docusaurus/registry.js
@@ -0,0 +1,4 @@
+export default {
+  "1f391b9e": [() => import(/* webpackChunkName: "1f391b9e" */ "@theme/MDXPage"), "@theme/MDXPage", require.resolveWeak("@theme/MDXPage")],
+  "a7456010": [() => import(/* webpackChunkName: "a7456010" */ "@generated/docusaurus-plugin-content-pages/default/__plugin.json"), "@generated/docusaurus-plugin-content-pages/default/__plugin.json", require.resolveWeak("@generated/docusaurus-plugin-content-pages/default/__plugin.json")],
+  "f3976560": [() => import(/* webpackChunkName: "f3976560" */ "@site/src/pages/index.md"), "@site/src/pages/index.md", require.resolveWeak("@site/src/pages/index.md")],};
