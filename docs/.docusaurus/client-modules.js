f5bdfa779ebd0f19ab7842cd4155d591d0f3d9ab
Arjun <arjunpa2001@gmail.com>
2024-09-12 15:52:02 +0530

    Test commit

--- docs/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("D:\\texttosql\\demo\\Readme-Test\\docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\texttosql\\demo\\Readme-Test\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\texttosql\\demo\\Readme-Test\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-];
