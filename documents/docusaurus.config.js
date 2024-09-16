a2b5d286b44faffae284dd3098c8b42bdb9eb9f6
Arjun <arjunpa2001@gmail.com>
2024-09-16 15:05:24 +0530

    Added Docs

--- documents/docusaurus.config.js
+++ documents/docusaurus.config.js
@@ -8,20 +8,19 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Raggenie',
+  // tagline: 'Dinosaurs are cool'
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://arjunPA2001.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Readme-Test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ArjunPA2001', // Usually your GitHub org/user name.
+  projectName: 'Readme-Test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
