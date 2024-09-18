e30e3b5382e91939ca54a3018a5f0442437422fc
Arjun <arjunpa2001@gmail.com>
2024-09-18 15:02:35 +0530

    updated docs

--- documents/docusaurus.config.js
+++ documents/docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: false
+        blog: false,
         // {
         //   showReadingTime: true,
         //   feedOptions: {
