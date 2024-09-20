254f3167efd6bd868aefee0db92bd375acf48ac5
Arjun <arjunpa2001@gmail.com>
2024-09-20 18:31:36 +0530

    updated docs

--- documents/docusaurus.config.js
+++ documents/docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ArjunPA2001/Readme-Test/tree/main/documents',
         },
         blog:
         {
@@ -55,7 +55,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ArjunPA2001/Readme-Test/tree/main/documents',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
