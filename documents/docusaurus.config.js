f12218b1fc0aac1cb78790760d6d47f2a787b567
Arjun <arjunpa2001@gmail.com>
2024-09-30 17:28:40 +0530

    Updated docs

--- documents/docusaurus.config.js
+++ documents/docusaurus.config.js
@@ -45,22 +45,7 @@ const config = {
           editUrl:
             'https://github.com/ArjunPA2001/Readme-Test/tree/main/documents',
         },
-        blog:
-        {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ArjunPA2001/Readme-Test/tree/main/documents',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -86,7 +71,6 @@ const config = {
             position: 'left',
             label: 'Documents',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -118,10 +102,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/sirocco-ventures/raggenie',
